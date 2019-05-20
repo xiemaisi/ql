6ea57dcc5abb29fc2c1ddad85e029df657f1e665
Max Schaefer <max@semmle.com>
2019-05-20 17:16:47 +0100

    JavaScript: Extend import target guessing based on absolute paths from AMD imports to other imports.

--- javascript/ql/test/library-tests/AMD/tst4.js
+++ javascript/ql/test/library-tests/AMD/tst4.js
@@ -1,6 +1,6 @@
 define([
         'a.js',      // not resolved: ambiguous
-        'foo',       // resolved to `lib/foo.js`
+        'foo',       // not resolved: too short for positive identification
         'nested/a',  // resolved to `lib/nested/a.js`
         'lib/foo.js' // resolved to `lib/foo.js`
        ], function(a, b, exports) {
